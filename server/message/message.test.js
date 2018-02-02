b0ac8702740378b3b71b96396758b880fc36adee
Abhinav Verma <abhicsmnnit@gmail.com>
2018-02-02 17:19:03 +0530

    Add test case for generateLocationMessage

--- server/message/message.test.js
+++ server/message/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const { generateMessage } = require('./message');
+const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate proper message', () => {
@@ -8,8 +8,21 @@ describe('generateMessage', () => {
 
         const message = generateMessage(from, text);
 
-        expect(message.from).toBe(from);
-        expect(message.text).toBe(text);
+        expect(message).toMatchObject({ from, text });
         expect(typeof message.createdAt).toBe('number');
     });
 });
+
+describe('generateLocationMessage', () => {
+    it('should generate proper geolocation message', () => {
+        const from = 'Some user';
+        const latitude = 1.2;
+        const longitude = 1.3;
+        const url = `https://google.com/maps?q=${latitude},${longitude+1}`;
+
+        const message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message).toMatchObject({ from, url });
+        expect(typeof message.createdAt).toBe('number');
+    });
+}); 
